e10bcc02907975de5c62f77684ad6e0100022d1a
Bala Paranj <bparanj@onekingslane.com>
2016-11-26 11:35:04 -0800

    Remove outer if conditional check to enforce quality value can never exceed 50 by using extracted increment_quality_of method

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -32,37 +32,31 @@ class GildedRose
   private
   
   def update(item)
-    if (item.name == "Aged Brie" || item.name == "Backstage passes to a TAFKAL80ETC concert")
-      if item.quality < 50
-        item.quality = item.quality + 1
-        if (item.name == "Backstage passes to a TAFKAL80ETC concert")
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
-          end
+    if (item.name == AGED_BRIE || item.name == BACKSTAGE_PASSES)
+      increment_quality_of(item)
+      if item.name == BACKSTAGE_PASSES
+        if item.sell_in < 11
+          increment_quality_of(item)
+        end
+        if item.sell_in < 6
+          increment_quality_of(item)
         end
       end
     elsif item.quality > 0
-      item.quality = item.quality - 1
+      item.quality -= 1
     end
-    item.sell_in = item.sell_in - 1
+    item.sell_in -= 1
     if item.sell_in < 0
       if item.name != "Aged Brie"
         if item.name != "Backstage passes to a TAFKAL80ETC concert"
           if item.quality > 0
-            item.quality = item.quality - 1
+            item.quality -= 1
           end
         else
           item.quality = item.quality - item.quality
         end
       elsif item.quality < 50
-        item.quality = item.quality + 1
+        item.quality += 1
       end
     end
   end
