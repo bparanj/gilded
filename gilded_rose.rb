5de1ca44111516fda2bf044eee539a60581a92d3
Bala Paranj <bparanj@onekingslane.com>
2016-11-26 00:56:07 -0800

    Extracted decrement_quality_of method

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -21,15 +21,21 @@ class GildedRose
     @items << Item.new("Conjured Mana Cake", 3, 6)
   end
 
+  # Backstage passes, like aged brie, increases in Quality as it's SellIn
+  #     value approaches; Quality increases by 2 when there are 10 days or less
+  #     and by 3 when there are 5 days or less but Quality drops to 0 after the
+  #     concert
   def update_quality
     for i in 0..(@items.size-1)
       if (@items[i].name != AGED_BRIE && @items[i].name != BACKSTAGE_PASSES)
         if (@items[i].quality > 0)
+          # "Sulfuras", being a legendary item, never decreases in Quality
           if (@items[i].name != SULFURAS)
-            @items[i].quality -= 1
+            decrement_quality_of(@items[i])
           end
         end
       else
+        # The Quality of an item is never more than 50
         if (@items[i].quality < 50)
           @items[i].quality += 1
           if (@items[i].name == BACKSTAGE_PASSES)
@@ -46,26 +52,36 @@ class GildedRose
           end
         end
       end
+      # "Sulfuras", being a legendary item, never has to be sold 
       if (@items[i].name != SULFURAS)
         @items[i].sell_in -= 1;
       end
+      # Expired item
       if (@items[i].sell_in < ZERO_DAYS)
+        # "Aged Brie" actually increases in Quality the older it gets
         if (@items[i].name != AGED_BRIE)
           if (@items[i].name != BACKSTAGE_PASSES)
             if (@items[i].quality > 0)
+              # "Sulfuras", being a legendary item, never decreases in Quality
               if (@items[i].name != SULFURAS)
-                @items[i].quality -= 1
+                decrement_quality_of(@items[i])
               end
             end
           else
+            # Once the sell by date has passed, Quality degrades twice as fast
             @items[i].quality = @items[i].quality - @items[i].quality
           end
         else
+          # The Quality of an item is never more than 50
           if (@items[i].quality < 50)
             @items[i].quality += 1
           end
         end
       end
     end
+  end
+  
+  def decrement_quality_of(item)
+    item.quality -= 1
   end
 end
