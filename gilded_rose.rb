3ebb29be1b226c92dcf1e9226765123946cfe5a8
Bala Paranj <bparanj@onekingslane.com>
2016-11-26 00:24:33 -0800

    Replaced hard-coded strings with constants

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,30 +1,34 @@
 require_relative 'item.rb'
 
 class GildedRose
+  BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert'
+  SULFURAS = 'Sulfuras, Hand of Ragnaros'
+  AGED_BRIE = 'Aged Brie'
+  
   @items = []
 
   def initialize
     @items = []
     @items << Item.new("+5 Dexterity Vest", 10, 20)
-    @items << Item.new("Aged Brie", 2, 0)
+    @items << Item.new(AGED_BRIE, 2, 0)
     @items << Item.new("Elixir of the Mongoose", 5, 7)
-    @items << Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
-    @items << Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
+    @items << Item.new(SULFURAS, 0, 80)
+    @items << Item.new(BACKSTAGE_PASSES, 15, 20)
     @items << Item.new("Conjured Mana Cake", 3, 6)
   end
 
   def update_quality
     for i in 0..(@items.size-1)
-      if (@items[i].name != "Aged Brie" && @items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+      if (@items[i].name != AGED_BRIE && @items[i].name != BACKSTAGE_PASSES)
         if (@items[i].quality > 0)
-          if (@items[i].name != "Sulfuras, Hand of Ragnaros")
+          if (@items[i].name != SULFURAS)
             @items[i].quality = @items[i].quality - 1
           end
         end
       else
         if (@items[i].quality < 50)
           @items[i].quality = @items[i].quality + 1
-          if (@items[i].name == "Backstage passes to a TAFKAL80ETC concert")
+          if (@items[i].name == BACKSTAGE_PASSES)
             if (@items[i].sell_in < 11)
               if (@items[i].quality < 50)
                 @items[i].quality = @items[i].quality + 1
@@ -38,14 +42,14 @@ class GildedRose
           end
         end
       end
-      if (@items[i].name != "Sulfuras, Hand of Ragnaros")
+      if (@items[i].name != SULFURAS)
         @items[i].sell_in = @items[i].sell_in - 1;
       end
       if (@items[i].sell_in < 0)
-        if (@items[i].name != "Aged Brie")
-          if (@items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+        if (@items[i].name != AGED_BRIE)
+          if (@items[i].name != BACKSTAGE_PASSES)
             if (@items[i].quality > 0)
-              if (@items[i].name != "Sulfuras, Hand of Ragnaros")
+              if (@items[i].name != SULFURAS)
                 @items[i].quality = @items[i].quality - 1
               end
             end
