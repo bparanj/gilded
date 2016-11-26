223a7c9081a4dcf03a626c449f7967ce39e789a3
Bala Paranj <bparanj@onekingslane.com>
2016-11-26 02:02:17 -0800

    Extract Sulfuras to a positive conditional

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -23,27 +23,26 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
-      if (item.name != AGED_BRIE && item.name != BACKSTAGE_PASSES)
+      if item.name == SULFURAS
+        # "Sulfuras", being a legendary item, never has to be sold 
         # "Sulfuras", being a legendary item, never decreases in Quality
-        if item.name != SULFURAS
-          decrement_quality_of(item)
-        end
       else
-        increment_quality_of(item)
-        # Backstage passes increases in Quality as it's SellIn
-        # value approaches; Quality increases by 2 when there are 10 days or less
-        # and by 3 when there are 5 days or less but Quality drops to 0 after the
-        # concert
-        if item.name == BACKSTAGE_PASSES
-          update_quality_for_backstage_passes(item)
+        if (item.name != AGED_BRIE && item.name != BACKSTAGE_PASSES)
+          decrement_quality_of(item)
+        else
+          increment_quality_of(item)
+          # Backstage passes increases in Quality as it's SellIn
+          # value approaches; Quality increases by 2 when there are 10 days or less
+          # and by 3 when there are 5 days or less but Quality drops to 0 after the
+          # concert
+          if item.name == BACKSTAGE_PASSES
+            update_quality_for_backstage_passes(item)
+          end
         end
-      end
-      # "Sulfuras", being a legendary item, never has to be sold 
-      if item.name != SULFURAS
         decrement_sell_in_days_for(item)
-      end
-      if expired?(item)
-        handle_expired(item)
+        if expired?(item)
+          handle_expired(item)
+        end        
       end
     end
   end
