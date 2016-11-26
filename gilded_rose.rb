fbf66ef823decef4ef75a67db6ae8789b119a11f
Bala Paranj <bparanj@onekingslane.com>
2016-11-26 11:21:58 -0800

    Removed nested if by using elsif

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -33,43 +33,38 @@ class GildedRose
   
   def update(item)
     if (item.name == "Aged Brie" || item.name == "Backstage passes to a TAFKAL80ETC concert")
-      if (item.quality < 50)
+      if item.quality < 50
         item.quality = item.quality + 1
         if (item.name == "Backstage passes to a TAFKAL80ETC concert")
-          if (item.sell_in < 11)
-            if (item.quality < 50)
+          if item.sell_in < 11
+            if item.quality < 50
               item.quality = item.quality + 1
             end
           end
-          if (item.sell_in < 6)
-            if (item.quality < 50)
+          if item.sell_in < 6
+            if item.quality < 50
               item.quality = item.quality + 1
             end
           end
         end
       end
-    else
-      if (item.quality > 0)
-         item.quality = item.quality - 1
-      end      
+    elsif item.quality > 0
+      item.quality = item.quality - 1
     end
-    item.sell_in = item.sell_in - 1;
-    if (item.sell_in < 0)
-      if (item.name != "Aged Brie")
-        if (item.name != "Backstage passes to a TAFKAL80ETC concert")
-          if (item.quality > 0)
-              item.quality = item.quality - 1
+    item.sell_in = item.sell_in - 1
+    if item.sell_in < 0
+      if item.name != "Aged Brie"
+        if item.name != "Backstage passes to a TAFKAL80ETC concert"
+          if item.quality > 0
+            item.quality = item.quality - 1
           end
         else
           item.quality = item.quality - item.quality
         end
-      else
-        if (item.quality < 50)
-          item.quality = item.quality + 1
-        end
+      elsif item.quality < 50
+        item.quality = item.quality + 1
       end
     end
-  
   end
   
   def decrement_sell_in_days_for(item)
